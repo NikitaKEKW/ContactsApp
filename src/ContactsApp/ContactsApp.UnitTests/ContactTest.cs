76a7cef3ae1f1815eaf6c7a279c10edc5c46cc64
NikitaKEKW <nik6@mail.ru>
2023-05-30 21:50:52 +0700

    features(ContactTest.cs, ProjectTest.cs): реализованы юнит-тесты

---
+++ src/ContactsApp/ContactsApp.UnitTests/ContactTest.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using NUnit.Framework;
+using ContactsApp.Model;
+
+namespace ContactsApp.UnitTests
+{
+    [TestFixture]
+    public class ContactTest
+    {
+        private Contact _contact;
+
+        [SetUp]
+        public void InitContact()
+        {
+            _contact = new Contact();
+        }
+
+        [TestCase("Смирнов", "Геттер FullName возвращает неправильную фамилию",
+            TestName = "Позитивный тест геттера FullName")]
+        public void FullName_SetCorrectFullName_ReturnsFullName(string fullName, string message)
+        {
+            //Arrange
+            var expected = fullName;
+            _contact.FullName = expected;
+
+            //Act
+            var actual = _contact.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual, message);
+        }
+
+        [TestCase("asdf@mail.ru", "Геттер Email возвращает неправильную фамилию",
+            TestName = "Позитивный тест геттера Email")]
+        public void Email_SetCorrectEmail__ReturnsEmail(string email, string message)
+        {
+            //Arrange
+            var expected = email;
+            _contact.Email = expected;
+
+            //Act
+            var actual = _contact.Email;
+
+            //Assert
+            Assert.AreEqual(expected, actual, message);
+        }
+
+        [TestCase("+79680695924", "Геттер Phone возвращает неправильный номер телефона",
+            TestName = "Позитивный тест геттера Phone")]
+        [TestCase("+7(968)0695924", "Геттер Phone возвращает неправильный номер телефона",
+            TestName = "Позитивный тест геттера Phone")]
+        [TestCase("89680695924", "Геттер Phone возвращает неправильный номер телефона",
+            TestName = "Позитивный тест геттера Phone")]
+        [TestCase("+7(968)069-59-24", "Геттер Phone возвращает неправильный номер телефона",
+            TestName = "Позитивный тест геттера Phone")]
+        [TestCase("+7968069-59-24", "Геттер Phone возвращает неправильный номер телефона",
+            TestName = "Позитивный тест геттера Phone")]
+        public void Phone_SetCorrectPhone_ReturnsPhone(string phone, string message)
+        {
+            //Arrange
+            var expected = phone;
+            _contact.PhoneNumber = expected;
+
+            //Act
+            var actual = _contact.PhoneNumber;
+
+            //Assert
+            Assert.AreEqual(expected, actual, message);
+        }
+
+        [TestCase(2000, 10, 10, "Геттер DateOfBirth возвращает неправильную фамилию",
+            TestName = "Позитивный тест геттера DateOfBirth")]
+        public void DateOfBirth_SetCorrectDateOfBirth_ReturnsDateOfBirth(int year, int month, int day, string message)
+        {
+            //Arrange
+            var expected = new DateTime(year, month, day);
+            _contact.DateOfBirth = expected;
+
+            //Act
+            var actual = _contact.DateOfBirth;
+
+            //Assert
+            Assert.AreEqual(expected, actual, message);
+        }
+
+        [TestCase("@superid01", "Геттер IdVk возвращает неправильную фамилию",
+            TestName = "Позитивный тест геттера IdVk")]
+        public void IdVk_SetCorrectIdVk_ReturnsIdVk(string idVk, string message)
+        {
+            //Arrange
+            var expected = idVk;
+            _contact.VkId = expected;
+
+            //Act
+            var actual = _contact.VkId;
+
+            //Assert
+            Assert.AreEqual(expected, actual, message);
+        }
+
+        [TestCase("Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов" +
+            "-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов-Смирнов" +
+            "-Смирнов-Смирнов-Смирнов-Смирнов-",
+            "Должно возникать исключение, если полное имя длиннее 100 символов",
+            TestName = "Присвоение неправильного полного имени больше 100 символов")]
+        public void FullName_SetTooLongFullName_ThrowsException(string wrongSurname, string message)
+        {
+            //Assert
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    //Act
+                    _contact.FullName = wrongSurname;
+                },
+                message);
+        }
+
+        [TestCase("testtesttesttesttesttesttesttesttesttesttesttest" +
+            "testtesttesttesttesttesttesttesttesttesttesttesttest" +
+            "testtesttesttesttesttest@mail.ru",
+            "Должно возникать исключение если почта длиннее 100 символов",
+            TestName = "Присвоение неправильной почты больше 100 символов")]
+        public void Email_SetTooLongEmail_ThrowsException(string wrongEmail, string message)
+        {
+
+            //Assert
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    //Act
+                    _contact.Email = wrongEmail;
+                },
+                message);
+        }
+
+        [TestCase("", "Должно возникать исключение, если номер телефона пустой",
+            TestName = "Присвоение пустой строки в качестве номера телефона")]
+        [TestCase("89234427925S!Й", "Должно возникать исключение, если номер телефона содержит символы",
+            TestName = "Присвоение чисел с символами в качестве номера телефона")]
+        [TestCase("7(8)1-924", "Должно возникать исключение, если номер телефона не полный",
+            TestName = "Присвоение не шаблонного номера телефона")]
+        [TestCase("7(8123)123-123-5412", "Должно возникать исключение, если номер телефона не полный",
+            TestName = "Присвоение не шаблонного номера телефона")]
+        [TestCase("7((*8123)123-123-5412", "Должно возникать исключение, если номер телефона содержит" +
+            "некорректные символы",
+            TestName = "Присвоение не шаблонного номера телефона")]
+        [TestCase("7(813)()123--13-12", "Должно возникать исключение, если номер телефона " +
+            "содержит лишние символы",
+            TestName = "Присвоение не шаблонного номера телефона")]
+        public void Phone_SetPhoneInWrongFormat_ThrowsException(string wrongNumber, string message)
+        {
+
+            //Assert
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    //Act
+                    _contact.PhoneNumber = wrongNumber;
+                },
+                message);
+        }
+
+        [TestCase(2222, 1, 1,
+            "Должно возникать исключение, если год рождения больше текущего",
+            TestName = "Присвоение года рождения больше текущего")]
+        [TestCase(1888, 1, 1,
+            "Должно возникать исключение,если год рождения меньше 1900 года",
+            TestName = "Присвоение года рождения менее 1900 года")]
+        public void DateOfBirth_SetDateOfBirthInWrongFormat_ThrowsException(int year, int month, int day, string message)
+        {
+            //Arrange
+            var wrongDateOfBirth = new DateTime(year, month, day);
+
+            //Assert
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    //Act
+                    _contact.DateOfBirth = wrongDateOfBirth;
+                },
+                message);
+        }
+
+        [TestCase("@id-id-id-id-id-id-id-id-id-id-id-id-id-id-id-id-id-id-id",
+            "Должно возникать исключение если ID длиннее 40 символов",
+            TestName = "Присвоение неправильного ID больше 40 символов")]
+        public void IdVk_SetTooLongIdVk_ThrowsExceptionn(string wrongIdVk, string message)
+        {
+
+            //Assert
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    //Act
+                    _contact.VkId = wrongIdVk;
+                },
+                message);
+        }
+    }
+}
---
+++ src/ContactsApp/ContactsApp.UnitTests/ProjectTest.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using ContactsApp.Model;
+
+namespace ContactsApp.UnitTests
+{
+    [TestFixture]
+    public class ProjectTest
+    {
+        private Project _project;
+
+        private Project _expected;
+
+        private Project _actual;
+
+        [SetUp]
+        public void InitProject()
+        {
+            _project = new Project();
+            _expected = new Project();
+            _actual = new Project();
+        }
+
+        [TestCase("Метод сортировки контактов вернул неверные значения списка контактов",
+            TestName = "Тестирование сортировки списка контактов")]
+        public void Contacts_SortContactsByFullName_ReturnsValue(string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"),
+                new Contact(
+                    "B",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2010, 12, 12),
+                    "@test"),
+                new Contact(
+                    "A",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2001, 12, 12),
+                    "@test")
+            };
+
+            _expected.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "A",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2001, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "B",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2010, 12, 12),
+                    "@test"),
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test")
+            };
+
+            //Act
+            _actual.Contacts = _project.SortContactsByFullName(_project.Contacts);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts[0].FullName,_actual.Contacts[0].FullName, message);
+        }
+
+        [TestCase("Метод сортировки контактов вернул неверные значения списка контактов",
+            TestName = "Тестирование сортировки пустого списка контактов")]
+        public void Contacts_SortContactsWithoutValues_ReturnsValue(string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>();
+            _expected.Contacts = new List<Contact>();
+
+            //Act
+            _actual.Contacts = _project.SortContactsByFullName(_project.Contacts);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts,_actual.Contacts, message);
+        }
+
+        [TestCase("Метод сортировки контактов вернул неверные значения списка контактов",
+            TestName = "Тестирование уже отсортированного списка контактов")]
+        public void Contacts_SortContactsByFullNameWithSortedListContacts_ReturnsValue(
+            string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "A",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "B",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    )
+            };
+
+            _expected.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "A",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "B",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"),
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test")
+            };
+
+            //Act
+            _actual.Contacts = _project.SortContactsByFullName(_project.Contacts);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts[0].FullName,
+                _actual.Contacts[0].FullName, message);
+        }
+
+        [TestCase("Метод поиска именинников вернул неверное значения списка контактов",
+            TestName = "Тестирование поиска именинников")]
+        public void Contacts_FindBirthdayContacts_ReturnsValue(string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "A",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "B",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    DateTime.Today,
+                    "@test"
+                    )
+            };
+
+            _expected.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    DateTime.Today,
+                    "@test"
+                    ),
+            };
+
+            //Act
+            _actual.Contacts = _project.FindBirthdayContacts(_project.Contacts);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts[0].FullName,_actual.Contacts[0].FullName, message);
+        }
+
+        [TestCase("Метод поиска именинников вернул неверное значения списка контактов",
+            TestName = "Тестирование поиска именинников если сегодня их нет")]
+        public void Contacts_FindBirthdayContactsWithoutBirthdayContacts_ReturnsValue(string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "A",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "B",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    )
+            };
+
+            _expected.Contacts = new List<Contact>();
+
+            //Act
+            _actual.Contacts = _project.FindBirthdayContacts(_project.Contacts);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts,_actual.Contacts, message);
+        }
+
+        [TestCase("Метод поиска именинников вернул неверное значения списка контактов",
+            TestName = "Тестирование поиска именинников если список контактов пустой")]
+        public void Contacts_FindBirthdayContactsWithEmptyListContacts_ReturnsValue(string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>();
+
+            _expected.Contacts = new List<Contact>();
+
+            //Act
+            _actual.Contacts = _project.FindBirthdayContacts(_project.Contacts);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts,_actual.Contacts, message);
+        }
+
+        [TestCase("Смирнов", "Метод поиска контактов возвращает неверные значения списка контактов",
+            TestName = "Тестирование поиска контактов")]
+        public void Contacts_FindContacts_ReturnsValue(string contactName, string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "Смирнов",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "B",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    )
+            };
+
+            _expected.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "Смирнов",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    )
+            };
+
+            //Act
+            _actual.Contacts = _project.FindContaсts(_project.Contacts, contactName);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts[0].FullName,_actual.Contacts[0].FullName, message);
+        }
+
+        [TestCase("Смирнов", "Метод поиска контактов возвращает неверные значения списка контактов",
+            TestName = "Тестирование поиска контактов в пустом списке контактов")]
+        public void Contacts_FindContactsWithEmptyListContacts_ReturnsValue(string contactName, string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>();
+
+            _expected.Contacts = new List<Contact>();
+
+            //Act
+            _actual.Contacts = _project.FindContaсts(_project.Contacts, contactName);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts,_actual.Contacts, message);
+        }
+
+        [TestCase("", "Метод поиска контактов возвращает неверные значения списка контактов",
+            TestName = "Тестирование поиска контактов с пустым искомым контактом")]
+        public void Contacts_FindContactsWithEmptySubstring_ReturnsValue(string contactName, string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "Смирнов",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "B",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    )
+            };
+
+            _expected.Contacts = new List<Contact>()
+            {
+                new Contact(
+                    "Смирнов",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "B",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    ),
+                new Contact(
+                    "C",
+                    "asdf@mail.ru",
+                    "+79680695924",
+                    new DateTime(2000, 12, 12),
+                    "@test"
+                    )
+            };
+
+            //Act
+            _actual.Contacts = _project.FindContaсts(_project.Contacts, contactName);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts[0].FullName, _actual.Contacts[0].FullName, message);
+        }
+
+        [TestCase("", "Метод поиска контактов возвращает неверные значения списка контактов",
+            TestName = "Тестирование поиска контактов в пустом списке контактов" +
+            "с пустым искомым контактом")]
+        public void Contacts_FindContactsWithEmptyListContactsWithoutSubstring_ReturnsValue(string contactName, string message)
+        {
+            //Arrange
+            _project.Contacts = new List<Contact>();
+
+            _expected.Contacts = new List<Contact>();
+
+            //Act
+            _actual.Contacts = _project.FindContaсts(_project.Contacts, contactName);
+
+            //Assert
+            Assert.AreEqual(_expected.Contacts,_actual.Contacts, message);
+        }
+    }
+}
